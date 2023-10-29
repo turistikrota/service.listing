98de42b56403ff72eb8ba93420d432087d19bf3d
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-10-30 00:58:17 +0300

    feature/owner-nickname: complete the command

--- app/command/post_update_owner_nick_name.go
+++ app/command/post_update_owner_nick_name.go
@@ -1,1 +1,20 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+)
+
+type PostUpdateOwnerNickNameCommand struct{}
+
+type PostUpdateOwnerNickNameRes struct{}
+
+type PostUpdateOwnerNickNameHandler cqrs.HandlerFunc[PostUpdateOwnerNickNameCommand, *PostUpdateOwnerNickNameRes]
+
+func NewPostUpdateOwnerNickNameHandler() PostUpdateOwnerNickNameHandler {
+	return func(ctx context.Context, cmd PostUpdateOwnerNickNameCommand) (*PostUpdateOwnerNickNameRes, *i18np.Error) {
+		return &PostUpdateOwnerNickNameRes{}, nil
+	}
+}
