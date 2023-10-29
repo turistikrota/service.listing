e19c239e9fe5b454f83d5f4394afe52c5cf6450a
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-10-30 01:00:16 +0300

    feature/validated: add the command

--- app/command/post_validated.go
+++ app/command/post_validated.go
@@ -1,1 +1,29 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.post/domains/account"
+	"github.com/turistikrota/service.post/domains/post"
+)
+
+type PostValidatedCommand struct {
+	New     *post.Entity   `json:"-"`
+	Account account.Entity `json:"-"`
+}
+
+type PostValidatedRes struct{}
+
+type PostValidatedHandler cqrs.HandlerFunc[PostValidatedCommand, *PostValidatedRes]
+
+func NewPostValidatedHandler(factory post.Factory, repo post.Repository, events post.Events) PostValidatedHandler {
+	return func(ctx context.Context, cmd PostValidatedCommand) (*PostValidatedRes, *i18np.Error) {
+		err := repo.Update(ctx, cmd.New)
+		if err != nil {
+			return nil, err
+		}
+		return &PostValidatedRes{}, nil
+	}
+}
