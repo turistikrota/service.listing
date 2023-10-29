0422296666a5e1289b42693c52baec3c4ff795d2
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-10-30 00:55:15 +0300

    feature/enable: add the command

--- app/command/post_enable.go
+++ app/command/post_enable.go
@@ -1,1 +1,36 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.post/domains/account"
+	"github.com/turistikrota/service.post/domains/post"
+)
+
+type PostEnableCommand struct {
+	Account  account.Entity `json:"-"`
+	PostUUID string         `json:"-"`
+}
+
+type PostEnableRes struct{}
+
+type PostEnableHandler cqrs.HandlerFunc[PostEnableCommand, *PostEnableRes]
+
+func NewPostEnableHandler(factory post.Factory, repo post.Repository, events post.Events) PostEnableHandler {
+	return func(ctx context.Context, cmd PostEnableCommand) (*PostEnableRes, *i18np.Error) {
+		err := repo.Enable(ctx, cmd.PostUUID)
+		if err != nil {
+			return nil, err
+		}
+		events.Enabled(post.EnabledEvent{
+			UUID: cmd.PostUUID,
+			Account: post.AccountEvent{
+				UUID: cmd.Account.UUID,
+				Name: cmd.Account.Name,
+			},
+		})
+		return &PostEnableRes{}, nil
+	}
+}
