222af3b4998f26d0252f0c2aa4b5baf0e9196dbe
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-10-30 00:52:17 +0300

    feature/disable: add disable command

--- app/command/post_disable.go
+++ app/command/post_disable.go
@@ -1,1 +1,36 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.post/domains/account"
+	"github.com/turistikrota/service.post/domains/post"
+)
+
+type PostDisableCommand struct {
+	Account  account.Entity `json:"-"`
+	PostUUID string         `json:"-"`
+}
+
+type PostDisableRes struct{}
+
+type PostDisableHandler cqrs.HandlerFunc[PostDisableCommand, *PostDisableRes]
+
+func NewPostDisableHandler(factory post.Factory, repo post.Repository, events post.Events) PostDisableHandler {
+	return func(ctx context.Context, cmd PostDisableCommand) (*PostDisableRes, *i18np.Error) {
+		err := repo.Disable(ctx, cmd.PostUUID)
+		if err != nil {
+			return nil, err
+		}
+		events.Disabled(post.DisabledEvent{
+			UUID: cmd.PostUUID,
+			Account: post.AccountEvent{
+				UUID: cmd.Account.UUID,
+				Name: cmd.Account.Name,
+			},
+		})
+		return &PostDisableRes{}, nil
+	}
+}
