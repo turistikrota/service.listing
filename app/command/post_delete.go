e4b1a4e55a16e9481a613a5cb022b4c5c7d2c17f
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-10-30 00:48:19 +0300

    feature/delete: complete delete command

--- app/command/post_delete.go
+++ app/command/post_delete.go
@@ -1,1 +1,36 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.post/domains/account"
+	"github.com/turistikrota/service.post/domains/post"
+)
+
+type PostDeleteCommand struct {
+	Account  account.Entity `json:"-"`
+	PostUUID string         `json:"-"`
+}
+
+type PostDeleteRes struct{}
+
+type PostDeleteHandler cqrs.HandlerFunc[PostDeleteCommand, *PostDeleteRes]
+
+func NewPostDeleteHandler(factory post.Factory, repo post.Repository, events post.Events) PostDeleteHandler {
+	return func(ctx context.Context, cmd PostDeleteCommand) (*PostDeleteRes, *i18np.Error) {
+		err := repo.Delete(ctx, cmd.PostUUID)
+		if err != nil {
+			return nil, err
+		}
+		events.Deleted(post.DeletedEvent{
+			UUID: cmd.PostUUID,
+			Account: post.AccountEvent{
+				UUID: cmd.Account.UUID,
+				Name: cmd.Account.Name,
+			},
+		})
+		return &PostDeleteRes{}, nil
+	}
+}
