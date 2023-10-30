93108f916c41ed366813dd538687b47e886d73bf
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-10-30 17:10:17 +0300

    add the post create http handler

--- server/http/api.go
+++ server/http/api.go
@@ -1,1 +1,33 @@
 package http
+
+import (
+	"github.com/cilloparch/cillop/middlewares/i18n"
+	"github.com/cilloparch/cillop/result"
+	"github.com/gofiber/fiber/v2"
+	"github.com/turistikrota/service.post/app/command"
+	"github.com/turistikrota/service.post/domains/account"
+	"github.com/turistikrota/service.post/domains/post"
+	"github.com/turistikrota/service.shared/server/http/auth/current_account"
+	"github.com/turistikrota/service.shared/server/http/auth/current_owner"
+)
+
+func (h srv) PostCreate(ctx *fiber.Ctx) error {
+	cmd := command.PostCreateCommand{}
+	h.parseBody(ctx, &cmd)
+	a := current_account.Parse(ctx)
+	o := current_owner.Parse(ctx)
+	cmd.Account = account.Entity{
+		UUID: a.ID,
+		Name: a.Name,
+	}
+	cmd.Owner = post.Owner{
+		UUID:     o.UUID,
+		NickName: o.NickName,
+	}
+	res, err := h.app.Commands.PostCreate(ctx.UserContext(), cmd)
+	if err != nil {
+		l, a := i18n.GetLanguagesInContext(h.i18n, ctx)
+		return result.Error(h.i18n.TranslateFromError(*err, l, a))
+	}
+	return result.SuccessDetail(Messages.Success.PostCreated, res)
+}
--- server/http/msg.go
+++ server/http/msg.go
@@ -1,1 +1,27 @@
 package http
+
+type successMessages struct {
+	PostCreated string
+}
+
+type errorMessages struct {
+	RequiredAuth      string
+	CurrentUserAccess string
+	AdminRoute        string
+}
+
+type messages struct {
+	Success successMessages
+	Error   errorMessages
+}
+
+var Messages = messages{
+	Success: successMessages{
+		PostCreated: "http_success_post_created",
+	},
+	Error: errorMessages{
+		RequiredAuth:      "http_error_required_auth",
+		CurrentUserAccess: "http_error_current_user_access",
+		AdminRoute:        "http_error_admin_route",
+	},
+}
