aa26d68edd6a91dd175627ef2946aff774c82c71
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-10-30 19:43:59 +0300

    feature/delete: add http handler

--- server/http/api.go
+++ server/http/api.go
@@ -91,3 +91,21 @@ func (h srv) PostDisable(ctx *fiber.Ctx) error {
 	}
 	return result.SuccessDetail(Messages.Success.Ok, res)
 }
+
+func (h srv) PostDelete(ctx *fiber.Ctx) error {
+	detail := command.PostDetailCmd{}
+	a := current_account.Parse(ctx)
+	h.parseParams(ctx, &detail)
+	cmd := command.PostDeleteCmd{}
+	cmd.PostUUID = detail.PostUUID
+	cmd.Account = account.Entity{
+		UUID: a.ID,
+		Name: a.Name,
+	}
+	res, err := h.app.Commands.PostDelete(ctx.UserContext(), cmd)
+	if err != nil {
+		l, a := i18n.GetLanguagesInContext(h.i18n, ctx)
+		return result.Error(h.i18n.TranslateFromError(*err, l, a))
+	}
+	return result.SuccessDetail(Messages.Success.Ok, res)
+}
