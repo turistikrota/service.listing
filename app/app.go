e5adf792e4c1c3c5800846f81571888d12bad606
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-10-30 14:12:17 +0300

    feature/app: add the cqrs app

--- app/app.go
+++ app/app.go
@@ -1,1 +1,30 @@
 package app
+
+import (
+	"github.com/turistikrota/service.post/app/command"
+	"github.com/turistikrota/service.post/app/query"
+)
+
+type Application struct {
+	Commands Commands
+	Queries  Queries
+}
+
+type Commands struct {
+	PostCreate              command.PostCreateHandler
+	PostUpdate              command.PostUpdateHandler
+	PostValidated           command.PostValidatedHandler
+	PostUpdateOwnerNickName command.PostUpdateOwnerNickNameHandler
+	PostEnable              command.PostEnableHandler
+	PostDisable             command.PostDisableHandler
+	PostDelete              command.PostDeleteHandler
+	PostRestore             command.PostRestoreHandler
+	PostReOrder             command.PostReOrderHandler
+}
+
+type Queries struct {
+	PostView          query.PostViewHandler
+	PostAdminView     query.PostAdminViewHandler
+	PostFilterByOwner query.PostFilterByOwnerHandler
+	PostListMy        query.PostListMyHandler
+}
